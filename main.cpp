95920a07eeb302213cf8f554e0c8ea57b5814b05
user <working54321@gmail.com>
2018-12-12 17:09:28 -0500

    added basic heap srot functionallity, inplce of course

--- main.cpp
+++ main.cpp
@@ -16,12 +16,15 @@ int numberOfProcessorsToUse;
 int* valuesToSort;
 
 bool debug=true;
+
+///allocateInitalMemory: simply does most of malloc needed for program.
 void  allocateInitalMemory(){
 	int n = numberOfValuesToSort;
 	
 	valuesToSort = (int *)malloc(sizeof(int) * n); 
 }
 
+///importFromFile: simply imports text file (in specifc format) into array.
 void importFromFile(string filename){
 	
 	ifstream inputFile;
@@ -53,6 +56,7 @@ void importFromFile(string filename){
 	inputFile.close();
 }
   
+///exportToFile: simply prints sorted array and performance data.
 void exportToFile(string filename){
 	
 	fstream outputFile;
@@ -76,7 +80,55 @@ void exportToFile(string filename){
 	outputFile.close();
 }
 
+///maxHeapifier(): recursive function that ensure value at location i follows,
+///				   Heap Sort rules, and adjusts accordingly until array is in
+///				   proper format.
+void maxHeapifer(int index, int n){
+	int i_leftnode = (index * 2) +1;
+	int i_rightnode = (index * 2) +2;
+	int i_largest = index;
+	
+	///See if child is larger than parent, if so, record the larger child
+	if((i_leftnode <= n) and (valuesToSort[i_leftnode] > valuesToSort[index])){
+		i_largest = i_leftnode;
+	}
+	
+	if((i_rightnode <= n) and (valuesToSort[i_rightnode] > valuesToSort[i_largest])){
+		i_largest = i_rightnode;
+	}
+	
+	///if a Heap Sort rule is violated adjust the array
+	if(i_largest != index){
+		int temp = valuesToSort[index];
+		valuesToSort[index] = valuesToSort[i_largest];
+		valuesToSort[i_largest] = temp;
+		
+		///because we swapped, we need to make sure values under the uzew
+		///swapped value follow Heap Sort
+		maxHeapifer(i_largest, n);
+	}
+}
 
+///maxHeapMaker(): creates the inital max heap needed for Heap Sort
+void maxHeapMaker(int n){
+	for(int i= ceil(n/2)-1; i >= 0; i--){
+		maxHeapifer(i, n-1);
+	}
+}
+
+///heapSorter(): the controller function to launch a Heap Sort
+void heapSorter(int n){
+	maxHeapMaker(n);
+	
+	///after making the max heap, perform the classic bottom-leaf swap and adjust
+	int temp = -1;
+	for(int i = n-1; i >= 0; i--){
+		temp = valuesToSort[i];
+		valuesToSort[i] = valuesToSort[0];
+		valuesToSort[0] = temp;
+		maxHeapifer	(0, i-1);
+	}
+}
 
 int main(int argc, char *argv[])
 
@@ -90,8 +142,10 @@ int main(int argc, char *argv[])
   p = MPI::COMM_WORLD.Get_size(); 
   rank = MPI::COMM_WORLD.Get_rank();
   
-  
-  
+ 
+ //Step One: Locally Sort each processors values
+ 
+  if(rank==0){
 	string inputfile = "input-0" + to_string(rank) + ".txt";
 	string outputfile = "output-0" + to_string(rank) + ".txt";
 	
@@ -100,11 +154,13 @@ int main(int argc, char *argv[])
 	importFromFile(inputfile);
 	
 	if(debug==true){cout << endl;}
+	int n = numberOfValuesToSort;
+	heapSorter(n);
 	
 	if(debug==true){cout << "Output File: "<< rank << endl;}
 	exportToFile(outputfile);
 
-  
+}
   
   //Exit
   MPI::Finalize();
